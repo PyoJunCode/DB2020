c8f7438b32cdf13f79d4719c0e887884cdea6000
서준표 <seojunpyo@seojunpyoui-MacBookPro.local>
2020-05-10 07:45:28 +0900

    first commit

---
+++ db.py
@@ -0,0 +1,184 @@
+# coding: utf-8
+import requests
+import re
+import sys
+from bs4 import BeautifulSoup
+from os import system
+from urllib.parse import urlencode, quote
+from fake_useragent import UserAgent
+import pymysql
+
+# 21800370 seo jun pyo DB team project
+
+
+getfake = lambda: getdateinfo().replace(' ', '%20')
+
+urlencode_noquote = lambda query: urlencode(query, quote_via = lambda k,l,m,n: k)
+
+class Lecture:
+
+
+    _CAMOUFLAGE_CHROME = {'User-Agent' : UserAgent().chrome}
+
+    HISNET_ROOT = 'http://hisnet.handong.edu'
+    # http://hisnet.handong.edu/login/_login.php
+    HISNET_LOGIN_PAGE = HISNET_ROOT + '/login/_login.php'
+    # http://hisnet.handong.edu/for_student/course/PLES430M.php?
+    HISNET_LECTURE_SEARCH_PAGE = HISNET_ROOT+'/for_student/course/PLES330M.php?'
+
+    db = pymysql.connect(host='52.14.37.173', port=3306, user='root', passwd='dba', db='Project', charset='utf8')
+    
+    cursor = db.cursor()
+
+
+    def __init__(self, idpw, date):
+        """ idpw : hisnet id/password
+            term : semester info
+            the_time : hot time
+            targets : target lectures
+            kakao_token : katalk info
+        """
+        self.s = requests.Session()
+        self.idpw = {
+            'id' :       idpw[0],
+            'password' : idpw[1],
+            'Language' : 'Korean'
+        }
+        self.date = date
+        
+
+        print('Sign in hisnet..')
+        resp = self.disguised_post(self.HISNET_LOGIN_PAGE, data=self.idpw)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        script_txt = soup.select('script')[0].text.strip()
+        if resp.status_code == 200:
+            print('SUCCESS !')
+        print('connect to DB server...')
+        if self.db:
+            print('SUCCESS !')
+    
+
+
+
+
+    def disguised_post(self, url, **kwargs):
+        return self.s.post(url, headers = self._CAMOUFLAGE_CHROME, **kwargs)
+
+    def disguised_get(self, url):
+        return self.s.get(url, headers = self._CAMOUFLAGE_CHROME)
+        
+
+        
+
+    def get_lecture_name(self,hakbu,date,open_id):
+        
+        hak_year = date[:-1]
+        hak_term = date[-1:]
+        
+        query = {
+            
+            'hak_year' : hak_year,
+            'hak_term' : hak_term,
+            'hakbu': hakbu,
+            'isugbn':'%C0%FC%C3%BC',
+            'injung':'%C0%FC%C3%BC',
+            'eng':'%C0%FC%C3%BC',
+            'gwamok_code':'',
+            'ksearch':'search'
+        }
+        #query['gwamok_code'] = lecture_code
+        
+        page = 1
+
+        
+        while page < 2 :
+        
+            lookup_lecture_url = self.HISNET_LECTURE_SEARCH_PAGE + 'Page='+ str(page)+ '&' +    urlencode_noquote(query)
+            resp = self.disguised_get(lookup_lecture_url)
+            soup = BeautifulSoup(resp.content, 'html.parser')
+            tbody_items = soup.find_all('table', border='0', width='750', cellspacing='0',  cellpadding='0', id='att_list')
+            
+            if(len(tbody_items[0]) < 5):
+                
+                return
+
+            for tr in tbody_items[0].select('tr')[1:]:
+
+               lec_code = tr.text.split('\n')[2]
+               sec_id = int(tr.text.split('\n')[3])
+               lec_name = tr.text.split('\n')[4]
+               credits = float(tr.text.split('\n')[5])
+               prof = tr.text.split('\n')[6]
+               time = tr.text.split('\n')[7]
+               building = tr.text.split('\n')[9]
+               total_stu = int(tr.text.split('\n')[10])
+               curr_stu = int(tr.text.split('\n')[11])
+               
+               check = "select `id` from `course` where course_code =%s limit 1"
+               self.cursor.execute(check,(lec_code))
+               result = self.cursor.fetchone()
+               if result is not None:
+                  course_id = result[0]
+               if result is None:
+                  sql = "INSERT INTO `course` (`course_code`, `title` ,`credits`,`major_code`) VALUES(%s, %s, %s, %s)"
+                  self.cursor.execute(sql,(lec_code,lec_name,credits, int(hakbu) ))
+                  self.db.commit()
+                  print('과목코드: ' + lec_code + ', lec_name: ' + lec_name + ', hakjum: ' + str(credits) )
+                  course_id = self.cursor.lastrowid
+               
+             
+               ssql = "INSERT INTO `section` (`open_id`, `course_id` ,`sec_id`,`building`, `time`,`prof_name`,`total_stu`,`curr_stu`) VALUES(%s,%s,%s,%s, %s, %s,%s,%s)"
+               self.cursor.execute(ssql,(open_id,course_id,sec_id,building,time,prof,total_stu,curr_stu))
+               print('분반: ' + str(sec_id) + ', time: ' + time )
+               #print('prof: ' + prof  + ', building: ' + building)
+               print()
+               
+            page=page + 1
+       
+
+
+                
+    def get_lecture_list(self):
+    
+    
+        lookup_lecture_url = self.HISNET_LECTURE_SEARCH_PAGE
+        resp = self.disguised_get(lookup_lecture_url)
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        major_list = soup.find('select', attrs={'name':'hakbu'})
+        gubun_list = soup.find('select', attrs={'name':''})
+        #print(major_list.select('option'))
+        
+        
+        for hak_term in self.date: # each date
+            print(hak_term)
+            
+            check = "select `open_id` from `open` where time =%s limit 1"
+            self.cursor.execute(check,(hak_term))
+            result = self.cursor.fetchone()
+            if result is not None:
+                open_id = result[0]
+            if result is None:
+                sql = "INSERT INTO `open` (`time`) VALUES(%s) "
+                self.cursor.execute(sql,(hak_term))
+                self.db.commit()
+                open_id = self.cursor.lastrowid
+            
+            for major in major_list.select('option')[1:]:
+                sql = "INSERT INTO `major` (`major_code`, `major_name`) VALUES(%s, %s) ON DUPLICATE KEY UPDATE major_code=VALUES(major_code), major_name =VALUES(major_name)"
+                self.cursor.execute(sql,(major.get('value'),major.text.strip()))
+                self.db.commit()
+
+                print(major.get('value'))
+                print(major.text.strip())
+                
+                self.get_lecture_name(major.get('value'),hak_term,open_id)
+    #
+    #        db.commit()
+    #        db.close()
+
+s = Lecture(('ghkdalswn312','minjoo12^^'),('20181','20182','20191','20192','20201',))
+#@todo delete id
+
+s.get_lecture_list()
+
+
---
+++ eval.py
@@ -0,0 +1,91 @@
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from time import sleep
+import re
+import pymysql
+
+#21800370 seojunpyo DB team project
+
+db = pymysql.connect(host='52.14.37.173', port=3306, user='root', passwd='dba', db='Project', charset='utf8')
+
+cursor = db.cursor()
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(executable_path=r'./chromedriver', chrome_options=options)
+
+
+driver.get('https://everytime.kr/login')
+
+driver.implicitly_wait(2)
+
+
+driver.find_element_by_name('userid').send_keys('potenpanda')
+driver.find_element_by_name('password').send_keys('fhrmdls123')
+
+
+driver.find_element_by_xpath('//*[@id="container"]/form/p[3]/input').click()
+
+driver.get('https://everytime.kr/lecture')
+
+sleep(5)
+driver.find_element_by_xpath('//*[@id="sheet"]/ul/li[3]/a').click()
+
+
+pre_count = 0
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+   
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  
+    sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);")
+    sleep(1)
+   
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+articles = soup.select('.article')
+
+for article in articles:
+    
+    open = re.findall(r'\d+', article.select('.semester')[0].text)
+    opencat = '20'+open[0]+open[1]
+    lec_name = article.select('h3')[0].text.split(':')[0].strip()
+    prof_name = article.select('h3')[0].text.split(':')[1].strip()
+    desc = article.select('.text')[0].text
+    rate = article.select('.on')
+    rating = int(re.findall(r'\d+', rate[0]['style'])[0]) / 20
+    
+
+    checkc = "select `id` from `course` where title LIKE %s"
+    cursor.execute(checkc,("%"+lec_name+"%"))
+    result = cursor.fetchone()
+    if result is None:
+        print(lec_name +' not founded')
+        continue
+    print(lec_name +' founded')
+    course_id = result[0]
+
+        
+    checko = "select `open_id` from `open` where time =%s"
+    cursor.execute(checko,(opencat))
+    result = cursor.fetchone()
+    if result is None:
+        continue
+    open_id = result[0]
+
+    sql = "INSERT INTO review (`course_id`,`open_id`,`prof_name`,`description`) VALUES(%s,%s,%s,%s)"
+    cursor.execute(sql,(course_id,open_id,prof_name,desc))
+    db.commit()
+    
+driver.quit()
